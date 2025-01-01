f2013e3d484ccadab7a4e283c0c2caa5f05fe8ec
DharmilG <foreafcbeta@gmail.com>
2025-01-01 18:26:44 +0530

    Update Details.jsx

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,9 +1,63 @@
+import { useParams } from "react-router-dom";
+import { games } from "./Games.jsx";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import '../App.css';
 
-export default function Details(title) {
+export function Details() {
+  const { title } = useParams();
+
+  const game = games
+    .flatMap((category) => category.titles)
+    .find((game) => game.name === title);
+
   return (
-    <div>
-      <h1>{title}</h1>
-      <img src={title.img} />
+    <div
+      style={{
+        backgroundColor: "#000",
+        fontFamily: "Verdana, Geneva, Tahoma, sans-serif",
+        color: "#f1f1f1",
+      }}
+    >
+      <Navbar />
+      <div style={{ padding: "20px" }}>
+        <h1>{game.name}</h1>
+        <div style={{ display: "flex", gap: "40px", alignItems: "center" }}>
+          <img
+            src={game.img}
+            alt={game.name}
+            style={{ width: "300px", height: "400px", borderRadius: "10px" }}
+          />
+          <div>
+            <p style={{ fontSize: "18px", lineHeight: "1.6" }}>
+              {game.description}
+            </p>
+            <p style={{ fontSize: "16px", color: "#aaa" }}>
+              Developers: {game.developers}
+            </p>
+          </div>
+        </div>
+        <div style={{ marginTop: "40px" }}>
+          <h2>Trailer</h2>
+          <iframe
+            width="560"
+            height="315"
+            src={game.trailer}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+      <Footer />
     </div>
-  )
+  );
+}
+
+export default function Info() {
+  return (
+    <>
+      <Details />
+    </>
+  );
 }
