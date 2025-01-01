c76f95865956a0e24b9ec7e10891eb6a838bfd06
DharmilG <foreafcbeta@gmail.com>
2025-01-01 10:37:38 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import '../App.css';
+import '/src/App.css';
 const dropdown_menu = [
   { name: "Action", link: "/" },
   { name: "Adventure", link: "/" },
