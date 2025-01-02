f66bba478a8504d893ae77390dbb1c083c898f58
DharmilG <foreafcbeta@gmail.com>
2025-01-02 08:58:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,25 @@ import Navbar from "./components/Navbar";
 import Games from "./components/Games";
 import Footer from "./components/Footer";
 import "./App.css";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <>
-      <div className="app">
-        <Navbar />
-        <Games />
-        <Footer />
-      </div>
-    </>
+    <div className={`app ${isLoading ? "loading" : ""}`}>
+      <Navbar />
+      <Games />
+      <Footer className="footer"/>
+    </div>
   );
 }
 
