2ca812947a1d97884e649ca8053680a7d6e38c51
DharmilG <foreafcbeta@gmail.com>
2025-01-03 17:34:47 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,16 +10,12 @@ const dropdown_menu = [
   { name: "Open World", link: "/" },
 ];
 
-export function Dropdown() {
+function Dropdown() {
   return (
     <div>
       {dropdown_menu.map((item, index) => {
         return (
-          <Link
-            key={index}
-            to={item.link}
-            className="dropdown-link"
-          >
+          <Link key={index} to={item.link} className="dropdown-link">
             {item.name}
           </Link>
         );
@@ -27,17 +23,18 @@ export function Dropdown() {
     </div>
   );
 }
-export default function Navbar() {
+
+function Navbar() {
   return (
     <div
       className="navbar"
       style={{
         height: "55px",
         fontFamily: "Verdana, Geneva, Tahoma, sans-serif",
-        backgroundColor: "rgb(50, 50, 50)",
+        backgroundColor: "rgb(10, 10, 10)",
         display: "flex",
         alignItems: "center",
-        justifyContent: "flex-start",
+        justifyContent: "space-between",
         padding: "0 20px",
       }}
     >
@@ -57,6 +54,7 @@ export default function Navbar() {
           color: "white",
           textDecoration: "none",
           fontWeight: "500",
+          flex: 1,
         }}
       >
         <Link to="/">Home</Link>
@@ -66,6 +64,39 @@ export default function Navbar() {
         </div>
         <Link to="/">Contact</Link>
       </div>
+      <div
+        className="search-bar"
+        style={{
+          display: "flex",
+          flex: 1,
+        }}
+      >
+        <input type="search" placeholder="Search" />
+      </div>
+      <div
+        className="right-navbar"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-end",
+          padding: "0 20px",
+          gap: "10px",
+        }}
+      >
+        <div className="login-div">
+          <Link to="/login">
+          <button className="login-button">LOG IN</button>
+          </Link>
+        </div>
+        <div className="signin-div">
+        <Link to="/signin">
+          <button className="signin-button">SIGN IN</button>
+        </Link>
+        </div>
+      </div>
     </div>
   );
 }
+
+export { Dropdown };
+export default Navbar;
