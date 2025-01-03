4e27075193878e8d66bc7bc8797984931ad8a51b
DharmilG <foreafcbeta@gmail.com>
2025-01-03 17:33:26 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Details from "./components/Details";
-
+import Login from "./components/Login.jsx";
+import Signin from "./components/Signin.jsx";
+import OrderInfo from "./components/Orderinfo.jsx";
 const root = createRoot(document.getElementById("root"));
 
 root.render(
@@ -13,6 +15,9 @@ root.render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/details/:title" element={<Details />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signin" element={<Signin/>} />
+        <Route path="/orderinfo" element={<OrderInfo />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
